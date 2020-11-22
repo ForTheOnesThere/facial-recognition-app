b94c2b42a207ef6e2eb6e5577f5fe170772a6794
ForTheOnesThere <jamescockayne1@gmail.com>
2020-11-22 21:05:41 +0000

    Added search bar panel.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,9 +8,9 @@ import logo from './circuit.png';
 const Logo = () => {
     return(
      <div className='ma4 mt0'>
-         <Tilt className="Tilt br2 shadow-2" options={{ max : 35 }} style={{ height: 250, width: 250 }} >
+         <Tilt className="Tilt br2 shadow-2" options={{ max : 35 }} style={{ height: 100, width: 100 }} >
             <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'space-around'}}className="Tilt-inner">
-                <img className="center" alt='logo' src={logo}/>
+                <img className="centerLogo" alt='logo' src={logo}/>
             </div>
          </Tilt>
      </div>
