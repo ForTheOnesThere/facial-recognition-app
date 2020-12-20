b9bdcb613ba1c13cdc70db411b297941cbea9dab
James Cockayne <jamescockayne1@gmail.com>
2020-12-20 18:53:19 +0000

    Added rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+            <div className='white f3'>
+                {'James, you are rank...'}
+            </div>
+            <div className='white f1'>
+                {'#1'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
