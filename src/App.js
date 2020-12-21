ae6ca8e2c94cccc0a926ce5025d6be6af77ad5bf
ForTheOnesThere <jamescockayne1@gmail.com>
2020-12-21 19:08:23 +0000

    Added props to detect input and button pressing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
+import { Component } from 'react';
 
 const particleParams = {
   particles: {
@@ -26,17 +27,36 @@ const particleParams = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonPress = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div className="App">
       <Particles params={particleParams} className='particles'/>
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonPress={this.onButtonPress}/>
       {/*<FaceRecognition />*/}
-    </div>
-  );
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
  
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
     return(
      <div>
          <p className='f3'>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
          </p>
          <div className='center'>
              <div className='pa4 br3 shadow-5 center form'>
-                <input type='text' className='f4 pa2 w-70 center'></input>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Go!</button>  
+                <input type='text' className='f4 pa2 w-70 center' onChange={props.onInputChange} ></input>
+                <button onClick={props.onButtonPress} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Go!</button>  
              </div>
              
          </div>
