b8288c8e212b14a190fb9d5942cf58bd8061903f
ForTheOnesThere <jamescockayne1@gmail.com>
2020-12-24 22:48:22 +0000

    Changed sign in component to be class based

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const SignIn = (props) => {
-    return(
+class SignIn extends React.Component {
+    render(){
+        
+        const { changeRoute } = this.props;
+
+        return(
         <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
             <main className="pa4 black-80">
-                <form className="measure">
+                <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
@@ -18,19 +22,20 @@ const SignIn = (props) => {
                     </fieldset>
                     <div className="">
                         <input 
-                            onClick={() => props.changeRoute('home')}
+                            onClick={() => changeRoute('home')}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                             type="submit" 
                             value="Sign In" 
                         />
                     </div>
                     <div className="lh-copy mt3">
-                        <p onClick={() => props.changeRoute('register')} href="#0" className="f6 link dim black db">Create Account</p>
+                        <p onClick={() => changeRoute('register')} href="#0" className="f6 link dim black db">Create Account</p>
                     </div>
-                </form>
+                </div>
             </main>
         </article>
-    )
+        ) 
+    }   
 }
 
 export default SignIn;
